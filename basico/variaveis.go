661a318b89dabc5f4c99b08af4b655c8600de45d
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2025-03-20 20:21:02 -0300

    Create variaveis.go

---
+++ basico/variaveis.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome string = "Giovanni" // Você declara o nome e depois o tipo da variavel
+	var versao float32 = 1.1
+	var idade int = 20
+	fmt.Println("Olá, Me chamo ", nome, " e tenho ", idade, " anos")
+	fmt.Println("Este programa está na versão ", versao)
+}
