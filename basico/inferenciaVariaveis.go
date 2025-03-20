c9b0e8a4a2a9b79fcb09874b6857e9c0db6ea35e
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2025-03-20 20:54:26 -0300

    Create inferenciaVariaveis.go

---
+++ basico/inferenciaVariaveis.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	nome := "Giovanni" //É possivel declarar variavel dessa forma, sem colocar o var na frente do nome e sem especificar o tipo dela
+	idade := 20
+	salario := 3.800
+
+	fmt.Println("Seu nome é ", nome, "você tem", idade, "anos e ganha", salario, "mês")
+}
