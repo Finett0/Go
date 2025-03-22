65a70ebdf9a84248d41e21693492e9fb2eb14fb8
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2025-03-22 10:30:26 -0300

    Create funcoes.go

---
+++ basico/funcoes.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	exibirIntroducao()
+	exibeMenu()
+
+	comando := lerComando()
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo...")
+	case 3:
+		fmt.Println("Saindo do programa...")
+		os.Exit(0)
+	default:
+		fmt.Println("Opção não encontrada.")
+		os.Exit(-1)
+
+	}
+
+}
+
+func exibirIntroducao() {
+	nome := "Giovanni"
+	versao := 1.1
+	fmt.Println("Olá sr ", nome)
+	fmt.Println("Esté programa está na versão", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("3- sair do programa")
+}
+
+func lerComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi: ", comandoLido)
+
+	return comandoLido
+
+}
