ff9f26171db2ac588fcef56204afa155fe30b535
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2025-04-12 14:02:42 -0300

    Update hello.go

--- basico/hello.go
+++ basico/hello.go
@@ -2,6 +2,6 @@ package main //é o pacote que contém o código que será executado quando o pr
 
 import "fmt" //Tras um pacote externo para dentro do seu código
 
-func main() { //Essa função é chamada automaticamente que o programa rodar 
+func main() { //Essa função é chamada automaticamente que o programa roda
 	fmt.Println("Hello World!")
 }
